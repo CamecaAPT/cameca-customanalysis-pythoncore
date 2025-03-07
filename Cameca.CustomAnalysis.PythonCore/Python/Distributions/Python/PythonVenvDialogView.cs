8b916e5957b65ae18edb20dadd8ea203c8acc4e2
Jim Payne <jim.payne@ametek.com>
2025-03-07 13:34:47 -0600

    Replace XAML with C# implementation
    XAML global loading causes issues in library code
    as when multiple referencing extensions load the
    same XAML class, the last will always overwrite
    the previous. The solution is to translate XAML to raw
    C# code to avoid the global BAML cache.

---
+++ Cameca.CustomAnalysis.PythonCore/Python/Distributions/Python/PythonVenvDialogView.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using CommunityToolkit.Mvvm.Input;
+using Prism.Services.Dialogs;
+
+namespace Cameca.CustomAnalysis.PythonCore;
+
+internal class PythonVenvDialogView : UserControl
+{
+	public PythonVenvDialogView()
+	{
+		Resources = BuildResources();
+		Dialog.SetWindowStyle(this, CreateWindowStyle());
+		Dialog.SetWindowStartupLocation(this, WindowStartupLocation.CenterScreen);
+		Content = BuildContent();
+	}
+
+	private static ResourceDictionary BuildResources() => new ResourceDictionary
+	{
+		["InvertBooleanConverter"] = new InvertBooleanConverter(),
+	};
+
+	private static Style CreateWindowStyle() => new Style(typeof(Window))
+	{
+		Setters =
+		{
+			new Setter(Window.TitleProperty, new Binding(nameof(PythonVenvDialogViewModel.Title))),
+			new Setter(Window.ResizeModeProperty, ResizeMode.NoResize),
+			new Setter(Window.ShowInTaskbarProperty, false),
+			new Setter(Window.SizeToContentProperty, SizeToContent.WidthAndHeight),
+			new Setter(Window.WindowStyleProperty, WindowStyle.ToolWindow),
+		},
+	};
+
+	private object BuildContent()
+	{
+		var gridResources = new ResourceDictionary
+		{
+			["TextBoxStyle"] = new Style(typeof(TextBox))
+			{
+				Setters =
+				{
+					new Setter(TextBoxBase.IsReadOnlyProperty, true),
+					new Setter(FontFamilyProperty, new FontFamily("Consolas")),
+					new Setter(BackgroundProperty, Brushes.Transparent),
+					new Setter(TextBoxBase.VerticalScrollBarVisibilityProperty, ScrollBarVisibility.Auto),
+					new Setter(TextBox.TextWrappingProperty, TextWrapping.Wrap),
+				},
+			},
+			[typeof(Button)] = new Style(typeof(Button))
+			{
+				Setters =
+				{
+					new Setter(WidthProperty, 80d),
+					new Setter(PaddingProperty, new Thickness(5d)),
+				},
+			},
+		};
+		return new Grid
+		{
+			Width = 800d,
+			Height = 650d,
+			RowDefinitions =
+			{
+				new RowDefinition { Height = GridLength.Auto, },
+				new RowDefinition { Height = GridLength.Auto, },
+				new RowDefinition { Height = GridLength.Auto, },
+				new RowDefinition { Height = new GridLength(1d, GridUnitType.Star) },
+				new RowDefinition { Height = GridLength.Auto, },
+			},
+			Resources = gridResources,
+			Children =
+			{
+				new TextBox
+					{
+						Style = (Style)gridResources["TextBoxStyle"],
+					}
+					.SetBindingEx(TextBox.TextProperty, new Binding(nameof(PythonVenvDialogViewModel.CmdStatementText)) { Mode = BindingMode.OneWay })
+					.SetGridRow(0),
+				new UniformGrid
+					{
+						HorizontalAlignment = HorizontalAlignment.Center,
+						Columns = 3,
+						Children =
+						{
+							new Button
+							{
+								Content = "Create",
+							}
+							.SetBindingEx(ButtonBase.CommandProperty, nameof(PythonVenvDialogViewModel.RunCommand)),
+							new Button
+							{
+								Content = "Delete",
+							}
+							.SetBindingEx(ButtonBase.CommandProperty, nameof(PythonVenvDialogViewModel.DeleteCommand)),
+							new Button
+							{
+								Content = "Cancel",
+							}
+							.SetBindingEx(ButtonBase.CommandProperty, nameof(PythonVenvDialogViewModel.RunCancelCommand)),
+						}
+					}
+					.SetGridRow(1),
+				new TextBlock
+					{
+						Text = "Creating the virtual environment might take some time. Please be patient and wait for the success message."
+					}
+					.SetGridRow(2),
+				new AppendingTextBox
+					{
+						AppendScrollBehavior = AppendScrollBehavior.FollowBottom,
+						Style = (Style)gridResources["TextBoxStyle"],
+					}
+					.SetBindingEx(AppendingTextBox.ItemsSourceProperty, nameof(PythonVenvDialogViewModel.TextContent))
+					.SetGridRow(3),
+				new UniformGrid
+					{
+						Columns = 2,
+						Rows = 1,
+						HorizontalAlignment = HorizontalAlignment.Right,
+						Children =
+						{
+							new Button
+								{
+									Content = "Ok",
+								}
+								.SetBindingEx(ButtonBase.CommandProperty, nameof(PythonVenvDialogViewModel.OkCommand))
+								.SetBindingEx(IsEnabledProperty, new Binding($"{nameof(PythonVenvDialogViewModel.RunCommand)}.{nameof(IAsyncRelayCommand.IsRunning)}")
+								{
+									Converter = (IValueConverter)Resources["InvertBooleanConverter"],
+								}),
+							new Button
+								{
+									Content = "Cancel",
+								}
+								.SetBindingEx(ButtonBase.CommandProperty, nameof(PythonVenvDialogViewModel.CancelCommand)),
+						}
+					}
+					.SetGridRow(4),
+			}
+		};
+	}
+}
