95039f8ad79f4cc21f661cdaeef9eb76f836ea03
Jim Payne <jim.payne@ametek.com>
2025-05-24 01:03:22 -0500

    Add a DefaultPyObjectProvider
    Just calls ToPython on the provided object. The most common
    way of converting from C# to Python, so we don't need to force
    every user to create this basic provider.

---
+++ Cameca.CustomAnalysis.PythonCore/Python/ObjectProviders/DefaultPyObjectProvider.cs
@@ -0,0 +1,15 @@
+﻿using Python.Runtime;
+
+namespace Cameca.CustomAnalysis.PythonCore;
+
+public class DefaultPyObjectProvider : IPyObjectProvider
+{
+	private readonly object? value;
+
+	public DefaultPyObjectProvider(object? value)
+	{
+		this.value = value;
+	}
+
+	public PyObject GetPyObject(PyModule scope) => value?.ToPython() ?? PyObject.None;
+}
