8ccc66fb7e3ca0b8fefe45d9f6116a834ae04a0d
Jim Payne <jim.payne@ametek.com>
2025-06-28 23:28:40 -0500

    Revert "Test if dialog view name change fixes issue"
    This reverts commit c8731d950114216d8a710e76b93902c26c403220.

--- Cameca.CustomAnalysis.PythonCore/Python/Distributions/Python/PythonVenvDialogView.cs
+++ Cameca.CustomAnalysis.PythonCore/Python/Distributions/Python/PythonVenvDialogView.cs
@@ -14,9 +14,9 @@ using Prism.Services.Dialogs;
 
 namespace Cameca.CustomAnalysis.PythonCore;
 
-internal class PythonVenvDialogView2 : UserControl
+internal class PythonVenvDialogView : UserControl
 {
-	public PythonVenvDialogView2()
+	public PythonVenvDialogView()
 	{
 		Resources = BuildResources();
 		Dialog.SetWindowStyle(this, CreateWindowStyle());
